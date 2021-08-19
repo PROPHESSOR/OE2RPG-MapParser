7aa7e7b0f68df5ce13134fcde40c4c23cf2ec5ca
PROPHESSOR <xenteze@gmail.com>
2021-08-19 15:16:24 +0300

    Добавил маппинги (Эл'Ландра)

--- mappings.json.js
+++ mappings.json.js
@@ -3,9 +3,14 @@ module.exports = {
     1: 1,
     2: 2,
     3: 3,
+    4: 4,
+    5: 5,
+    8: 8,
     16: 11, // Check
     18: 10,
+    23: 12,
     46: 39,
+    47: 22,
     48: 23,
     50: 24,
     51: 25, // Interpolation
@@ -17,13 +22,15 @@ module.exports = {
     59: 31, // Interpolation
     60: 32, // Interpolation
     61: 33, // Interpolation
-    62: 34, // Interpolation
+    62: 36,
     63: 35, // Interpolation
     64: 36, // Interpolation
     65: 37, // Interpolation
     66: 38,
     67: 39, // Interpolation
     68: 40,
+    69: 41,
+    70: 42,
     81: 43,
     82: 44,
     83: 45
