60d6fda651bc990c815f664b75d567774eda674e
PROPHESSOR <xenteze@gmail.com>
2021-08-19 14:55:22 +0300

    Добавил маппинги

--- mappings.json.js
+++ mappings.json.js
@@ -1,8 +1,30 @@
 module.exports = {
     0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    16: 11, // Check
+    18: 10,
     46: 39,
+    48: 23,
+    50: 24,
+    51: 25, // Interpolation
+    52: 26, // Interpolation
     53: 27,
+    54: 28,
+    57: 29, // Interpolation
+    58: 30, // Interpolation
+    59: 31, // Interpolation
+    60: 32, // Interpolation
+    61: 33, // Interpolation
+    62: 34, // Interpolation
+    63: 35, // Interpolation
+    64: 36, // Interpolation
+    65: 37, // Interpolation
     66: 38,
+    67: 39, // Interpolation
+    68: 40,
     81: 43,
+    82: 44,
     83: 45
 };
